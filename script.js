ddd3d47c4be578a18538facac984af8fb8e0ee4e
Jeremy <jpthomas501@gmail.com>
2021-08-05 16:44:42 -0400

    random message

---
+++ script.js
@@ -0,0 +1,25 @@
+let button1 = document.getElementById("num1");
+let button2 = document.getElementById("num2");
+let button3 = document.getElementById("num3");
+let button4 = document.getElementById("num4");
+let button5 = document.getElementById("num5");
+
+button1.onclick = function() {
+    //console.log("This button works");
+}
+
+button2.onclick = function() {
+    //console.log("This button works");
+}
+
+button3.onclick = function() {
+    //console.log("This button also works");
+}
+
+button4.onclick = function() {
+    //console.log("This button works as well");
+}
+
+button5.onclick = function() {
+    //console.log("This is the final button that should work");
+}
